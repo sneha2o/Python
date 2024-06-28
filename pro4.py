5aab19645549db7ad64622db5857eaec79998bb9
Sneha <155820544+sneha2o@users.noreply.github.com>
2024-06-28 18:06:35 +0530

    University Practical's (Unit-1 -- p4)

---
+++ pro4.py
@@ -0,0 +1,32 @@
+# Create a sequence of numbers using range datatype to display 1 to 30, with an increment of 2.
+
+def seq_num(a, b):
+    for i in range(a,b,2):
+        print("Sequence Number is: ",i)
+
+seq_num(1, 30)
+
+# output:
+# Sequence Number is:  1
+# Sequence Number is:  3
+# Sequence Number is:  5
+# Sequence Number is:  7
+# Sequence Number is:  9
+# Sequence Number is:  11
+# Sequence Number is:  13
+# Sequence Number is:  15
+# Sequence Number is:  17
+# Sequence Number is:  19
+# Sequence Number is:  21
+# Sequence Number is:  23
+# Sequence Number is:  25
+# Sequence Number is:  27
+# Sequence Number is:  29
+
+
+
+
+
+
+
+
