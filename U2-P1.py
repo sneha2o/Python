0318743dede1117fe7ffba4598506e40c08674d4
Sneha <155820544+sneha2o@users.noreply.github.com>
2024-07-12 19:27:23 +0530

    University Practical's (Unit-2 -- p1)

---
+++ U2-P1.py
@@ -0,0 +1,13 @@
+# Write a program to create one array from another array.
+
+Original_array = [1,2,3,4,5,6,7,8,9,10]
+
+new_array = Original_array.copy()
+
+print("Original Array Is: ", Original_array)
+
+print("New Array Is: ", new_array)
+
+# output:-
+# Original Array Is:  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# New Array Is:  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
