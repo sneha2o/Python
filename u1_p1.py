2d6a0f99f6044a6b2fe0e262f9153c0626b33d15
Sneha <155820544+sneha2o@users.noreply.github.com>
2024-09-16 19:34:11 +0530

    GU (Unit-1 = p1)

---
+++ u1_p1.py
@@ -0,0 +1,10 @@
+#Write a program to swap numbers without taking a temporary variable.
+
+a = int(input("Enter value of first variable: "))
+b = int(input("Enter value of second variable: "))
+
+a = a+b
+b = a-b
+a = a-b
+
+print("a is: ",a, "b is: ",b)
