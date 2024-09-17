dfd0daf04a3970835556da9f9b3896b0764bb0ff
Sneha <155820544+sneha2o@users.noreply.github.com>
2024-09-17 12:44:26 +0530

    GU(Unit-1 = p2)

---
+++ u1_p2.py
@@ -0,0 +1,14 @@
+#Write a program to display sum of two complex numbers.
+
+print("Enter two complex number in the form a+bj: ")
+
+p1 = complex(input())
+p2 = complex(input())
+
+print("sum = ", p1+p2)
+
+#Output:-
+#Enter two complex number in the form a+bj: 
+#9+2j
+#9+4j
+#sum =  (18+6j)
