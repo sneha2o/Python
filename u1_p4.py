93870fe7e35a0adfed72dc50564a1f1e79b7fcdb
Sneha <155820544+sneha2o@users.noreply.github.com>
2024-09-19 12:39:36 +0530

    GU(Unit-1 = p4)

---
+++ u1_p4.py
@@ -0,0 +1,10 @@
+#Create a sequence of no using range datatype to dsiplay 1 to 30 with increment of 2.
+
+start = 1
+stop =30
+step = 2
+
+print(list(range(start, stop, step)))
+
+#output;- [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29]
+
