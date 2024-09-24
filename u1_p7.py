bf08dc548a313be43dde054c26e46bf1d215579e
Sneha <155820544+sneha2o@users.noreply.github.com>
2024-09-25 00:11:02 +0530

    GU(Unit-1 = p7)

---
+++ u1_p7.py
@@ -0,0 +1,14 @@
+#Write a program to evaluates an expression given by the user at run time using eval() function.
+a = input("Enter math function to evaluate: ")
+b = eval(a)
+
+print("The result of you enter math function is: ", b)
+
+#Output:-
+#Enter math function to evaluate: 10+8-9*2-(10*2)
+#The result of you enter math function is:  -20
+
+
+
+
+
