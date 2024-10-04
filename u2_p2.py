c29bcfab69595f040e665b9fc59fbe7dafa72641
Sneha <155820544+sneha2o@users.noreply.github.com>
2024-10-04 16:58:32 +0530

    GU(Unit-2 = p2)

---
+++ u2_p2.py
@@ -0,0 +1,17 @@
+#Write a program to retrive, display and update only a range of elements from an array using indexing and slicing in arrays.
+
+a = [2,4,6,8,10]
+
+print("Full array: ")
+print(a)
+
+print("print array using index number: ")
+print(a[2])
+print("Print array using slicing: ")
+
+b = a[0:2]
+print(b)
+c = a[:3]
+print(c)
+d = a[0:4]
+print(d)
