3de77de1120d144fb0d05a4bca1e745a9fc2d277
Sneha <155820544+sneha2o@users.noreply.github.com>
2024-10-11 18:12:28 +0530

    GU(Unit-2 = p8)

---
+++ u2_p8.py
@@ -0,0 +1,13 @@
+#Write a program to pass a list to a function and display it.
+
+def my_func(food):
+    for x in food:
+        print(x)
+
+fruits = ["Apple", "Banana", "Mango"]
+my_func(fruits)
+
+#Output:-
+##Apple
+##Banana
+##Mango
